68a9e8e9b4b227466c11e30d8ddc5c3031020135
Aron Nielzén <aron.nielzen@gmail.com>
2025-10-12 15:45:53 +0000

    testing

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -2,8 +2,22 @@ use std::io;
 mod words;
 
 
-// Finds most common character in a list of words
-fn char_guesser(alphabet: &str, word_vector: &Vec<&str>) -> char {
+
+
+fn initial_word_sorter<'a>(word_length: usize, word_vector: &'a [&'static str]) -> Vec<&'a str> {
+    let mut new_word_vector = Vec::new();
+
+    for word in word_vector {
+        if word.len() == word_length {
+            new_word_vector.push(*word);
+        } 
+    }
+    new_word_vector
+}
+
+
+
+fn char_guesser_from_word_list(alphabet: &str, word_vector: &Vec<&str>) -> char {
     let mut highest_count = 0;
     let mut current_count = 0;
     let mut current_guess = 'a';
@@ -30,20 +44,9 @@ fn char_guesser(alphabet: &str, word_vector: &Vec<&str>) -> char {
 
 
 
-// Returns a list of references to words with conformed length, used to initialize the word list
-fn initial_word_sorter<'a>(word_length: usize, word_vector: &'a [&'static str]) -> Vec<&'a str> {
-    let mut new_word_vector = Vec::new();
 
-    for word in word_vector {
-        if word.len() == word_length {
-            new_word_vector.push(*word);
-        } 
-    }
-    new_word_vector
-}
 
-// Converts user inputted string to vector of (position, character) tuples
-fn position_vec_converter(position_string: &str) -> Vec<(usize, char)> {
+fn user_position_vec_converter(position_string: &str) -> Vec<(usize, char)> {
     let mut position_vector = Vec::new();
 
     for i in 0..position_string.len() {
@@ -82,41 +85,18 @@ fn positional_word_sorter<'b>(char_positioning_map: &Vec<(usize, char)>, word_ve
 
 
 fn main() {
-    println!("Welcome to Hangman Solver!\nPlease input word length:");
-    let mut user_input_word_length = String::new();
-    io::stdin().read_line(&mut user_input_word_length)
-        .expect("Failed to read line");
-    let word_length = user_input_word_length.trim().parse::<usize>()
-        .expect("Please enter a valid number");
 
+    let mut current_vec = initial_word_sorter(5, words::WORD_LIST);
+    println!("{:?}", current_vec);
 
+    let mut guess = char_guesser_from_word_list(words::SWEDISH_ALPHABET, &current_vec);
+    println!("{}", guess);
 
-    let current_vec = initial_word_sorter(word_length, &words::WORD_LIST);
+    let mut user_input_map = String::new();
+    io::stdin().read_line(&mut user_input_map);
+    println!("{}", user_input_map);
 
-    while true {
-
-        println!("Enter known positions (use _ for unknowns, e.g. s_a for s in first position and a in third):");
-        let mut position_input = String::new();
-
-
-        io::stdin().read_line(&mut position_input)
-            .expect("Failed to read line");
-
-
-        if position_input != "\n" {
-            let position_map_vec = position_vec_converter(&position_input);
-            let current_vec = positional_word_sorter(&position_map_vec, &current_vec);
-            let guess = char_guesser(words::SWEDISH_ALPHABET, &current_vec);
-            println!("Possible words: {:?}", current_vec);
-            println!("Best guess: {}", guess);
-        }
-        else {
-            let guess = char_guesser(words::SWEDISH_ALPHABET, &current_vec);
-            println!("Possible words: {:?}", current_vec);
-            println!("Best guess: {}", guess);
-        }
-    }
-    
-
+    let current_position_vec = user_position_vec_converter(&user_input_map);
+    println!("{:?}", current_position_vec);
     
 }
